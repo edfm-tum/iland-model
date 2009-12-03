a8af712eec832f0e23e440a5ccb1206f8c0b5481
Werner Rammer <werner.rammer@tum.de>
2009-12-03 15:37:26 +0000

    new version. Main issues: * fixed several issues with landscape initialization * calculation of utilizable radiation on a daily basis + new debug output * smaller GUI improvements (e.g. autoload last saved project)

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
-const char *version = "Alpha 1.1.0";
-const char *svn_revision = "323M";
+const char *version = "Alpha 0.1";
+const char *svn_revision = "328:329M";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
