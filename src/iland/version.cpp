4efb5a1297e9eaf5b40fcacf5916dccd8cd458c1
Werner Rammer <werner.rammer@tum.de>
2009-12-10 15:08:54 +0000

    new exe: various fixes in water cycle

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
 const char *version = "Alpha 0.1";
-const char *svn_revision = "331M";
+const char *svn_revision = "336M";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
