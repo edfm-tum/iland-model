8cc07a0a6298a50b5b396c21cf8e1914387ed9ef
Werner Rammer <werner.rammer@tum.de>
2010-02-10 08:13:32 +0000

    new exe.

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
 const char *version = "Alpha 0.1";
-const char *svn_revision = "342M";
+const char *svn_revision = "342:344M";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
