580fdfa1d7ebec42c70ac5fc89ee16dcd8c1610c
Werner Rammer <werner.rammer@tum.de>
2010-03-17 12:53:19 +0000

    bugfix release. Again, troubles with the LIPs and new species...

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
 const char *version = "Alpha 0.1";
-const char *svn_revision = "357M";
+const char *svn_revision = "362M";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
