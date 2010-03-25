49d26c02d30602a3390ca4972abb6c64ec995a68
Werner Rammer <werner.rammer@tum.de>
2010-03-25 08:24:45 +0000

    new version. (watercycle fix, output log filtering, ...)

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
 const char *version = "Alpha 0.1";
-const char *svn_revision = "362M";
+const char *svn_revision = "365M";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
