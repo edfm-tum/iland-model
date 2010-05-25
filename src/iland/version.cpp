b8454197be21dc28f729f32e0ef78153c0ae9604
Werner Rammer <werner.rammer@tum.de>
2010-05-25 09:18:43 +0000

    new executeable version. Both exes are built against Qt Version 4.6.

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
 const char *version = "Alpha 0.2";
-const char *svn_revision = "397M";
+const char *svn_revision = "404M";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
