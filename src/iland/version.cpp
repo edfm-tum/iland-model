2dd3c1f25b578488a017cf9bae3323755870a89e
Werner Rammer <werner.rammer@tum.de>
2010-05-30 19:45:06 +0000

    new exe: include general switches to globally enable/disable the lineraization of expressions, and to define the logLevel of the application.

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
 const char *version = "Alpha 0.2";
-const char *svn_revision = "426M";
+const char *svn_revision = "431M";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
