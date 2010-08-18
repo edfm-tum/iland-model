3edb2dfd432b9e8253e693ee328700e83ade99f3
Werner Rammer <werner.rammer@tum.de>
2010-08-18 12:48:03 +0000

    release candidate for version 0.4 see http://iland.boku.ac.at/release+notes+0.4 for release notes. important: updaet of species parameter table necessary

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
 const char *version = "Alpha 0.3";
-const char *svn_revision = "446M";
+const char *svn_revision = "456M";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
