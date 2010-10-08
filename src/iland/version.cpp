30ea6ac5f641667d561ea58bedef2aef6ee77c0f
Werner Rammer <werner.rammer@tum.de>
2010-10-08 09:22:15 +0000

    a number of small changes in the sapling-growth subsystem + implementation of external seed input.

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
-const char *version = "Alpha 0.4";
-const char *svn_revision = "468M";
+const char *version = "Alpha 0.3";
+const char *svn_revision = "472M";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
