058135166d1bce81c126b99af06bf15f84332fcc
Werner Rammer <werner.rammer@tum.de>
2010-11-04 18:09:12 +0000

    update containing primarily the regeneration fix (reineke)

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
 const char *version = "Alpha 0.3";
-const char *svn_revision = "506M";
+const char *svn_revision = "508M";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
