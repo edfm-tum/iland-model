a1823e53b86573a88184af387f9b44817d465587
Werner Rammer <werner.rammer@tum.de>
2010-12-03 21:58:55 +0000

    version 0.5 alpha including snag dynamics and soil carbon/nitrogen model.

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
 const char *version = "Alpha 0.3";
-const char *svn_revision = "531M";
+const char *svn_revision = "532M";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
