092c8cd4965d9641fe3fd5ac6ee76d4bb2e19604
Werner Rammer <werner.rammer@tum.de>
2010-12-16 12:35:14 +0000

    new version 0.5 RC1

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
-const char *version = "0.5 RC1";
-const char *svn_revision = "536";
+const char *version = "Alpha 0.3";
+const char *svn_revision = "541M";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
