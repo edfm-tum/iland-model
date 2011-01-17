c76e74f65438ca11107cbe07ce61475f4b2a3047
Werner Rammer <werner.rammer@tum.de>
2011-01-17 13:08:11 +0000

    version 0.5 contains updated water cycle (net radiation) and snag-decay calculation.

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
-const char *version = "0.5 RC 3";
-const char *svn_revision = "559M";
+const char *version = "0.5";
+const char *svn_revision = "562M";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
