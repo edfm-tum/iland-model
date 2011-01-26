cf37c3865b1b5c17f3298b2b2183f19e7d038cbe
Werner Rammer <werner.rammer@tum.de>
2011-01-26 08:39:42 +0000

    new version 0.6 alpha 2 * bugfixes * resource unit ids in outputs

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
-const char *version = "0.6 alpha 1";
-const char *svn_revision = "567M";
+const char *version = "0.6 alpha 2";
+const char *svn_revision = "570";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
