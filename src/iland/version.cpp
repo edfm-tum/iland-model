5501c669245fb3f9f2bd1d3d89dfa2539ba414b9
Werner Rammer <werner.rammer@tum.de>
2011-01-31 18:00:32 +0000

    new version 0.6 beta 4 * improved management functionality.

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
-const char *version = "0.6 alpha 3";
-const char *svn_revision = "573";
+const char *version = "0.6 alpha 4";
+const char *svn_revision = "580";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
