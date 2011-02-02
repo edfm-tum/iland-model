b191b7829bd397fea5a2f252cf6b3a057f03c342
Werner Rammer <werner.rammer@tum.de>
2011-02-02 09:58:41 +0000

    new version 0.6 alpha 5 bugfixes

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
-const char *version = "0.6 alpha 4";
-const char *svn_revision = "580";
+const char *version = "0.6 alpha 5";
+const char *svn_revision = "585";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
