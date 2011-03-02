2e7e1b8b4059656a677244f19d3e93b35ebac67f
Werner Rammer <werner.rammer@tum.de>
2011-03-02 19:45:10 +0000

    version 0.6 extensions w.r.t. to initializing saplings, dynamic stand outputs (std.dev.) and advanced options to start/stop also debug outputs.

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
-const char *version = "0.6 beta 2";
-const char *svn_revision = "597";
+const char *version = "0.6";
+const char *svn_revision = "600";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
