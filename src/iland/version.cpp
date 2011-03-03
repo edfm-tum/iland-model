88e05ff8806221059ec95212ae19c38fe9b65c07
Werner Rammer <werner.rammer@tum.de>
2011-03-03 10:48:58 +0000

    version 0.6.1 contains a couple of smaller fixes

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
-const char *version = "0.6";
-const char *svn_revision = "600";
+const char *version = "0.6.1";
+const char *svn_revision = "605";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
