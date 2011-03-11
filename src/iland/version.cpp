20cc8ad425db20773eb2581eadd64964675c58f6
Werner Rammer <werner.rammer@tum.de>
2011-03-11 14:32:10 +0000

    bugfix version 0.6.4

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
-const char *version = "0.6.3";
-const char *svn_revision = "609";
+const char *version = "0.6.4";
+const char *svn_revision = "613";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
