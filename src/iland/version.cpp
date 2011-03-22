96b3bcb1a5329ed26cb101a89f81e569b7fd54d5
Werner Rammer <werner.rammer@tum.de>
2011-03-22 08:14:17 +0000

    new version 0.6.7 contains bug fixes

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
-const char *version = "0.6.6";
-const char *svn_revision = "624";
+const char *version = "0.6.7";
+const char *svn_revision = "630";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
