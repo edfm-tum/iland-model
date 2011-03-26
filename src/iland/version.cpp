38c6ff83498f9f564beae5f060b22bd83cc37e39
Werner Rammer <werner.rammer@tum.de>
2011-03-26 15:25:51 +0000

    includes the movie "setViewport" function allowing movie generation...

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
-const char *version = "0.6.8";
-const char *svn_revision = "630";
+const char *version = "0.6.9";
+const char *svn_revision = "635";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
