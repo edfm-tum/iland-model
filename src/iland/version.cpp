f0b7085a2e4670a759e3321b654f97bd5c7b34b1
Werner Rammer <werner.rammer@tum.de>
2011-10-20 10:13:56 +0000

    Snapshot functions to save/load a vegetation snapshot (currently: trees, soil, snags). Some additional javascript functions to access the functionality.

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
 const char *version = "0.7.0";
-const char *svn_revision = "668";
+const char *svn_revision = "675";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
