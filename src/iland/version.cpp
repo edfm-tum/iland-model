def9246aff7b12111b2c99999fdbdb27cdb74160
Werner Rammer <werner.rammer@gmail.com>
2011-12-06 17:08:19 +0000

    Speed-Patch! fixing the missing "&" in the  const Grid<float> seedMap() const { return mSeedMap; } ///< access to the seedMap function. This dramatically improves the performance of the model. Lots of time was lost in duplicating the seedmaps for each species * resourceunit. in addition: limited max-size of the logging QPlainTextEdit; this could maybe stop iLand from crashing....

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
 const char *version = "0.7.3";
-const char *svn_revision = "700";
+const char *svn_revision = "704";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
