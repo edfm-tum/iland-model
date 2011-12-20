4ea219b31e91cbe52a48437ae17b43265e0e4b4d
Werner Rammer <werner.rammer@gmail.com>
2011-12-20 11:12:54 +0000

    fixed the flip of ASCII grids also for FloatGrid (specialization).

--- src/core/grid.cpp
+++ src/core/grid.cpp
@@ -7,7 +7,7 @@ QString gridToString(const FloatGrid &grid, const QChar sep, const int newline_a
 {
     QString res;
     int newl_counter = newline_after;
-    for (int y=0;y<grid.sizeY();y++) {
+    for (int y=grid.sizeY()-1;y>=0;--y) {
          for (int x=0;x<grid.sizeX();x++) {
             res+=QString::number(grid.constValueAtIndex(QPoint(x,y))) + sep;
             if (--newl_counter==0) {
