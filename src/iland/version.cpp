486ea6aa1e642833f86c24fd861f21bf669702ff
Werner Rammer <werner.rammer@tum.de>
2012-02-10 12:58:42 +0000

    first version of the iland wind module (0.8.0).

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
-const char *version = "0.7.3";
-const char *svn_revision = "719";
+const char *version = "0.8.0";
+const char *svn_revision = "723";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
