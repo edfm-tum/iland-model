7b6cd8a88dd5de4909ad58ac953e688fd1911f9c
Werner Rammer <werner.rammer@gmail.com>
2012-04-12 14:33:21 +0000

    changed calculation of "f_gap" in wind module. The critical wind speeds are now lower (there was a problem combining different approaches; now the f_gap is scaled to be 1 at gap size 0).

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
 const char *version = "0.8.1";
-const char *svn_revision = "737";
+const char *svn_revision = "740";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
