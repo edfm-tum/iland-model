247aaadc219b8f021c4818a3b1c9d0965e64e036
Werner Rammer <werner.rammer@gmail.com>
2012-04-18 07:30:15 +0000

    new version 0.8.2 includes topo modifier for wind module on 10m grid.

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
-const char *version = "0.8.1";
-const char *svn_revision = "740";
+const char *version = "0.8.2";
+const char *svn_revision = "743";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
