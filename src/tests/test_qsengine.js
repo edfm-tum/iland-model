5489ebdd9b1b518377c2a7d7142f9cdc2fd18830
Werner Rammer <werner.rammer@gmail.com>
2013-09-26 14:43:03 +0000

    added a test file for javascript

---
+++ src/tests/test_qsengine.js
@@ -0,0 +1,10 @@
+// test for some features of the Qt-API
+
+function test_factory()
+{
+	print("create CSVFile");
+	var csvfile = Factory.newCSVFile("asdf");
+	print(csvfile);
+	//var cc = Factory.newClimateConverter();
+	//print(cc);
+}
