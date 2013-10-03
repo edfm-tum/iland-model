f0f3781901c8a37a1acbe62dc4f43358be54e715
Werner Rammer <werner.rammer@tum.de>
2013-10-03 12:35:02 +0000

    new "round()" function in the expression engine

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
 const char *version = "0.8.506";
-const char *svn_revision = "791";
+const char *svn_revision = "800";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
