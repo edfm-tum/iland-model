4bdac57a4df9ee4e0d8a800cb57c21445febd81e
Werner Rammer <werner.rammer@tum.de>
2013-10-29 16:26:34 +0000

    changed the light calculation for sapling growth: instead of a generic 4m (for relative height: 4/height_dom_pixel), we use the *actual* height of the sapling. This change should make the competition more strong.

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
-const char *version = "0.8.506";
-const char *svn_revision = "800";
+const char *version = "0.8.507";
+const char *svn_revision = "811";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
