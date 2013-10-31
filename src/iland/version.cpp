92329ef04a8d8c4cc366fc52dff0485898237435
Werner Rammer <werner.rammer@tum.de>
2013-10-31 16:00:58 +0000

    port from qt5 branch gui improvements

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,4 +1,4 @@
-const char *version = "0.8.507";
-const char *svn_revision = "811";
+const char *version = "0.8.508";
+const char *svn_revision = "814";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
