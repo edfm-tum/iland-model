efb4b5fef4e4b7f0b54033a18d3bdade082effb8
Werner Rammer <werner.rammer@tum.de>
2013-11-12 12:39:19 +0000

    new executable with required Qt 5.1 files. version 0.8.6.

--- src/iland/version.cpp
+++ src/iland/version.cpp
@@ -1,6 +1,6 @@
 #include <QString>
-const char *version = "0.8.507";
-const char *svn_revision = "821";
+const char *version = "0.8.6";
+const char *svn_revision = "838";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
 
