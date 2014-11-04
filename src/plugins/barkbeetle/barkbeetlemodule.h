47e4303a373b65c01bea77ce525dea4f6ad89870
Werner Rammer <werner.rammer@tum.de>
2014-11-04 15:49:30 +0000

    lil' update

--- src/plugins/barkbeetle/barkbeetlemodule.h
+++ src/plugins/barkbeetle/barkbeetlemodule.h
@@ -1,12 +1,20 @@
 #ifndef BARKBEETLEMODULE_H
 #define BARKBEETLEMODULE_H
 
+#include "grid.h"
+
+class BarkBeetleCell
+{
+    int n;
+};
+
 class ResourceUnit; // forward
 
 class BarkBeetleModule
 {
 public:
     BarkBeetleModule();
+    static double cellsize() { return 10.; }
 
     void setup(); ///< general setup
     void setup(const ResourceUnit *ru); ///< setup for a specific resource unit
@@ -15,6 +23,8 @@ public:
     void run();
 
     void yearBegin();
+private:
+    Grid<BarkBeetleCell> mGrid;
 
 };
 
