843d037bf147d0a5e33fd254729ee570ba4034eb
Werner Rammer <werner.rammer@tum.de>
2016-02-11 13:21:52 +0000

    integrating sapling growth...

--- src/core/saplings.cpp
+++ src/core/saplings.cpp
@@ -9,6 +9,9 @@
 #include "species.h"
 #include "seeddispersal.h"
 
+double Saplings::mRecruitmentVariation = 0.1; // +/- 10%
+double Saplings::mBrowsingPressure = 0.;
+
 
 Saplings::Saplings()
 {
@@ -111,5 +114,154 @@ void Saplings::establishment(const ResourceUnit *ru)
         }
 
     }
+
+}
+
+void Saplings::saplingGrowth(const ResourceUnit *ru)
+{
+    HeightGrid *height_grid = GlobalSettings::instance()->model()->heightGrid();
+    FloatGrid *lif_grid = GlobalSettings::instance()->model()->grid();
+
+    QPoint imap =  mGrid.indexAt(ru->boundingBox().topLeft());
+    for (int iy=0; iy<cPxPerRU; ++iy) {
+        SaplingCell *s = mGrid.ptr(imap.x(), imap.y()+iy); // ptr to the row
+        int isc = mGrid.index(imap.x(), imap.y()+iy);
+
+        for (int ix=0;ix<cPxPerRU; ++ix, ++s, ++isc) {
+            if (s->state != SaplingCell::CellInvalid) {
+                for (int i=0;i<NSAPCELLS;++i) {
+                    if (s->saplings[i].is_occupied()) {
+                        // growth of this sapling tree
+                        const HeightGridValue &hgv = (*height_grid)[height_grid->index5(isc)];
+                        float lif_value = (*lif_grid)[isc];
+
+                        growSapling(ru, s->saplings[i], isc, hgv.height, lif_value);
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+void Saplings::updateBrowsingPressure()
+{
+    if (GlobalSettings::instance()->settings().valueBool("model.settings.browsing.enabled"))
+        Saplings::mBrowsingPressure = GlobalSettings::instance()->settings().valueDouble("model.settings.browsing.browsingPressure");
+    else
+        Saplings::mBrowsingPressure = 0.;
+}
+
+void Saplings::growSapling(const ResourceUnit *ru, SaplingTree &tree, int isc, float dom_height, float lif_value)
+{
+    ResourceUnitSpecies *rus = const_cast<ResourceUnitSpecies*>(ru->ruSpecies()[tree.species_index]);
+    const Species *species = rus->species();
+
+    // (1) calculate height growth potential for the tree (uses linerization of expressions...)
+    double h_pot = species->saplingGrowthParameters().heightGrowthPotential.calculate(tree.height);
+    double delta_h_pot = h_pot - tree.height;
+
+    // (2) reduce height growth potential with species growth response f_env_yr and with light state (i.e. LIF-value) of home-pixel.
+    if (dom_height==0.f)
+        throw IException(QString("growSapling: height grid at %1/%2 has value 0").arg(isc));
+
+    double rel_height = tree.height / dom_height;
+
+    double lif_corrected = species->speciesSet()->LRIcorrection(lif_value, rel_height); // correction based on height
+
+    double lr = species->lightResponse(lif_corrected); // species specific light response (LUI, light utilization index)
+
+    double delta_h_factor = rus->prod3PG().fEnvYear() * lr; // relative growth
+
+    if (h_pot<0. || delta_h_pot<0. || lif_corrected<0. || lif_corrected>1. || delta_h_factor<0. || delta_h_factor>1. )
+        qDebug() << "invalid values in Sapling::growSapling";
+
+    // check browsing
+    if (mBrowsingPressure>0. && tree.height<=2.f) {
+        double p = rus->species()->saplingGrowthParameters().browsingProbability;
+        // calculate modifed annual browsing probability via odds-ratios
+        // odds = p/(1-p) -> odds_mod = odds * browsingPressure -> p_mod = odds_mod /( 1 + odds_mod) === p*pressure/(1-p+p*pressure)
+        double p_browse = p*mBrowsingPressure / (1. - p + p*mBrowsingPressure);
+        if (drandom() < p_browse) {
+            delta_h_factor = 0.;
+        }
+    }
+
+    // check mortality of saplings
+    if (delta_h_factor < species->saplingGrowthParameters().stressThreshold) {
+        tree.stress_years++;
+        if (tree.stress_years > species->saplingGrowthParameters().maxStressYears) {
+            // sapling dies...
+            tree.clear();
+            rus->saplingStat().addCarbonOfDeadSapling( tree.height / species->saplingGrowthParameters().hdSapling * 100. );
+            return;
+        }
+    } else {
+        tree.stress_years=0; // reset stress counter
+    }
+    DBG_IF(delta_h_pot*delta_h_factor < 0.f || delta_h_pot*delta_h_factor > 2., "Sapling::growSapling", "inplausible height growth.");
+
+    // grow
+    tree.height += delta_h_pot * delta_h_factor;
+    tree.age++; // increase age of sapling by 1
+
+    // recruitment?
+    if (tree.height > 4.f) {
+        rus->saplingStat().mRecruited++;
+
+        float dbh = tree.height / species->saplingGrowthParameters().hdSapling * 100.f;
+        // the number of trees to create (result is in trees per pixel)
+        double n_trees = species->saplingGrowthParameters().representedStemNumber(dbh);
+        int to_establish = static_cast<int>( n_trees );
+
+        // if n_trees is not an integer, choose randomly if we should add a tree.
+        // e.g.: n_trees = 2.3 -> add 2 trees with 70% probability, and add 3 trees with p=30%.
+        if (drandom() < (n_trees-to_establish) || to_establish==0)
+            to_establish++;
+
+        // add a new tree
+        for (int i=0;i<to_establish;i++) {
+            Tree &bigtree = const_cast<ResourceUnit*>(ru)->newTree();
+
+            bigtree.setPosition(mGrid.indexOf(isc));
+            // add variation: add +/-10% to dbh and *independently* to height.
+            bigtree.setDbh(dbh * nrandom(1. - mRecruitmentVariation, 1. + mRecruitmentVariation));
+            bigtree.setHeight(tree.height * nrandom(1. - mRecruitmentVariation, 1. + mRecruitmentVariation));
+            bigtree.setSpecies( const_cast<Species*>(species) );
+            bigtree.setAge(tree.age,tree.height);
+            bigtree.setRU(const_cast<ResourceUnit*>(ru));
+            bigtree.setup();
+            const Tree *t = &bigtree;
+            const_cast<ResourceUnitSpecies*>(rus)->statistics().add(t, 0); // count the newly created trees already in the stats
+        }
+        // clear all regeneration from this pixel (including this tree)
+        tree.clear(); // clear this tree (no carbon flow to the ground)
+        SaplingCell &s=mGrid[isc];
+        for (int i=0;i<NSAPCELLS;++i) {
+            if (s.saplings[i].is_occupied()) {
+                // add carbon to the ground
+                rus->saplingStat().addCarbonOfDeadSapling( s.saplings[i].height / species->saplingGrowthParameters().hdSapling * 100.f );
+                s.saplings[i].clear();
+            }
+        }
+        return;
+    }
+    // book keeping (only for survivors) for the sapling of the resource unit / species
+    SaplingStat &ss = rus->saplingStat();
+    ss.mLiving++;
+    ss.mAvgHeight+=tree.height;
+    ss.mAvgAge+=tree.age;
+    ss.mAvgDeltaHPot+=delta_h_pot;
+    ss.mAvgHRealized += delta_h_pot * delta_h_factor;
+
+}
+
+void SaplingStat::clearStatistics()
+{
+    mRecruited=mDied=mLiving=0;
+    mSumDbhDied=0.;
+    mAvgHeight=0.;
+    mAvgAge=0.;
+    mAvgDeltaHPot=mAvgHRealized=0.;
 
 }
--- src/core/saplings.h
+++ src/core/saplings.h
@@ -2,15 +2,18 @@
 #define SAPLINGS_H
 
 #include "grid.h"
+#include "snag.h"
+
 
 struct SaplingTree {
-    SaplingTree() { age=0; species_index=0; stress_years=0; flags=0; height=0.f;  }
+    SaplingTree() { clear(); }
     short unsigned int age;  // number of consectuive years the sapling suffers from dire conditions
     short unsigned int species_index; // index of the species within the resource-unit-species container
     unsigned char stress_years; // (upper 16bits) + age of sapling (lower 16 bits)
     unsigned char flags;
     float height; // height of the sapling in meter
     bool is_occupied() const { return height>0.f; }
+    void clear()  {  age=0; species_index=0; stress_years=0; flags=0; height=0.f;  }
     void setSapling(const float h_m, const int age_yrs, const int species_idx) { height=h_m; age=static_cast<short unsigned int>(age_yrs); stress_years=0; species_index=static_cast<short unsigned int>(species_idx); }
 };
 #define NSAPCELLS 5
@@ -34,7 +37,49 @@ struct SaplingCell {
                       }
 };
 class ResourceUnit;
+class Saplings;
 
+class SaplingStat
+{
+public:
+    void clearStatistics();
+    // actions
+    void addCarbonOfDeadSapling(float dbh) { mDied++; mSumDbhDied+=dbh; }
+
+    // access to statistics
+    int newSaplings() const { return mAdded; }
+    int diedSaplings() const { return mDied; }
+    int livingSaplings() const { return mLiving; } ///< get the number
+    int recruitedSaplings() const { return mRecruited; }
+    ///  returns the *represented* (Reineke's Law) number of trees (N/ha) and the mean dbh/height (cm/m)
+    double livingStemNumber(double &rAvgDbh, double &rAvgHeight, double &rAvgAge) const;
+
+    double averageHeight() const { return mAvgHeight; }
+    double averageAge() const { return mAvgAge; }
+    double averageDeltaHPot() const { return mAvgDeltaHPot; }
+    double averageDeltaHRealized() const { return mAvgHRealized; }
+    /// return the number of trees represented by one sapling of the current species and given 'height'
+    double representedStemNumber(float height) const;
+    // carbon and nitrogen
+    const CNPair &carbonLiving() const { return mCarbonLiving; } ///< state of the living
+    const CNPair &carbonGain() const { return mCarbonGain; } ///< state of the living
+
+private:
+    int mAdded; ///< number of trees added
+    int mRecruited; ///< number recruited (i.e. grown out of regeneration layer)
+    int mDied; ///< number of trees died
+    double mSumDbhDied; ///< running sum of dbh of died trees (used to calculate detritus)
+    int mLiving; ///< number of trees (cohorts!!!) currently in the regeneration layer
+    double mAvgHeight; ///< average height of saplings (m)
+    double mAvgAge; ///< average age of saplings (years)
+    double mAvgDeltaHPot; ///< average height increment potential (m)
+    double mAvgHRealized; ///< average realized height increment
+    CNPair mCarbonLiving;
+    CNPair mCarbonGain; ///< net growth (kg / ru) of saplings
+
+    friend class Saplings;
+
+};
 
 class Saplings
 {
@@ -43,14 +88,23 @@ public:
     void setup();
     // main functions
     void establishment(const ResourceUnit *ru);
+    void saplingGrowth(const ResourceUnit *ru);
+
+
     void clearStats() { mAdded=0; mTested=0;}
     int saplingsAdded() const { return mAdded; }
     int pixelTested() const { return mTested; }
 
+    static void setRecruitmentVariation(const double variation) { mRecruitmentVariation = variation; }
+    static void updateBrowsingPressure();
+
 private:
+    void growSapling(const ResourceUnit *ru, SaplingTree &tree, int isc, float dom_height, float lif_value);
     Grid<SaplingCell> mGrid;
     int mAdded;
     int mTested;
+    static double mRecruitmentVariation;
+    static double mBrowsingPressure;
 };
 
 #endif // SAPLINGS_H
