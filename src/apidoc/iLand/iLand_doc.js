a7d095cc3db9ed36a684fd2c859ec1b3aa33bb1a
Werner Rammer <werner.rammer@gmail.com>
2016-07-08 13:48:15 +0000

    more documentaiton...

---
+++ src/apidoc/iLand/iLand_doc.js
@@ -0,0 +1,15 @@
+/**
+The individual based landscape and disturbance model
+
+This is the documentation of the iLand Javascript API.
+
+The model documentation as well the option to download the model can be found on the iLand [homepage](http://iland.boku.ac.at).
+
+Some wiki-pages that are more related to the Javascript API are:
++ general topics: http://iland.boku.ac.at/iLand+scripting
++ how to load Javascript code: http://iland.boku.ac.at/project+file#javascript
++ interactive Javascript and the iLand Viewer: http://iland.boku.ac.at/iLand+viewer
+
+@main iLand
+@module iLand
+*/
