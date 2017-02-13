050218bc8635a2b7d81aa84c33c2ad38a5df3a33
Werner Rammer <werner.rammer@gmail.com>
2017-02-13 12:56:25 +0000

    fixed an issue with small trees that are kept alive even if light response was 0 (and no NPP was generated).

--- src/core/version.cpp
+++ src/core/version.cpp
@@ -20,7 +20,7 @@
 
 #include <QString>
 static const char *version = "1.01";
-static const char *svn_revision = "1246";
+static const char *svn_revision = "1247";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
 
