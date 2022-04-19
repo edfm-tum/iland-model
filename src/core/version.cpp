713d0f1ed112732e42f90c99267b492415eabb44
Werner Rammer <werner.rammer@gmail.com>
2022-04-19 12:42:40 +0000

    fixed loop in printSuppressedWarnings

--- src/core/version.cpp
+++ src/core/version.cpp
@@ -20,7 +20,7 @@
 
 #include <QString>
 static const char *version = "1.1.2";
-static const char *svn_revision = "1459";
+static const char *svn_revision = "1460";
 const char *currentVersion(){ return version;}
 const char *svnRevision(){ return svn_revision;}
 
