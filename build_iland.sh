6ac093427f4560aa20e285a953aaf3329655cae7
Werner Rammer <werner.rammer@gmail.com>
2026-01-24 09:54:32 +0100

    working on github automation
    added windows freeImage.dll

--- build_iland.sh
+++ build_iland.sh
@@ -1,9 +1,10 @@
 # build iLand on Linux
 # release mode
 
-mkdir src/plugins/build/release
-mkdir src/iland/build/release
-mkdir src/ilandc/build/release
+mkdir -p src/plugins/build/release
+mkdir -p src/iland/build/release
+mkdir -p src/ilandc/build/release
+mkdir -p src/fonstudio/build/release
 
 cd src/plugins/build/release
 qmake ../../plugins.pro 
@@ -18,6 +19,12 @@ cd ../../../..
 cd src/ilandc/build/release
 qmake ../../ilandc.pro 
 make -j$(nproc 2>/dev/null || sysctl -n hw.ncpu)
+
+cd ../../../..
+cd src/fonstudio/build/release
+qmake ../../fonstudio.pro 
+make -j$(nproc 2>/dev/null || sysctl -n hw.ncpu)
+
 
 echo "==================================================================="
 echo "Executables in src/ilandc/build/release and src/iland/build/release"
